f5db0e15b85f466bc6127b45d7256c49fd8b0663
Bảo Nhân Hồ Sỹ <hosybaonhan2004@gmail.com>
2024-12-01 13:49:14 +0000

    System generated commit, initial commit

---
+++ pages/Dashboard/jsobjects/WarningProvince/WarningProvince.js
@@ -0,0 +1,61 @@
+export default {
+  myVar1: [],
+  myVar2: {},
+  async getWarningProvince() {
+    try {
+      const response = await nchmf_warnings.run(); // Gọi API
+      if (response && response.data) {
+				const data = [response.data[0]]
+        const processedData = data.map((item) => {
+          return {
+            id: item.id,
+            documentId: item.documentId,
+            date: item.date,
+            details: item.data.data.map((subItem) => ({
+              title: subItem.title,
+              date: subItem.date,
+              time: subItem.time,
+              link: subItem.link,
+              type: subItem.type,
+              region: subItem.region
+            }))
+          };
+        });
+
+        const groupedByProvince = {};
+        processedData.forEach((item) => {
+          item.details.forEach((detail) => {
+            detail.region.forEach((province) => {
+              if (!groupedByProvince[province]) {
+                groupedByProvince[province] = [];
+              }
+              groupedByProvince[province].push({
+                title: detail.title,
+                date: detail.date,
+                time: detail.time,
+                link: detail.link,
+                type: detail.type.join(", ")
+              });
+            });
+          });
+        });
+
+        this.myVar1 = groupedByProvince;
+			const result = Object.entries(groupedByProvince)
+				.map(([province, warnings]) => ({
+					province,
+					warnings,
+				}))
+				.sort((a, b) => b.warnings.length - a.warnings.length);
+
+
+        return result;
+      } else {
+        throw new Error("No data found in response");
+      }
+    } catch (error) {
+      console.error("Error loading data:", error);
+      return {};
+    }
+  }
+};
